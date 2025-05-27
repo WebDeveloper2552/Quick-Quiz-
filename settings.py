f642e6a879b5cbe0cd16cc9879d52cdc54695423
WebDeveloper2552 <ivana.zaykova@aoc.co.uk>
2025-05-27 21:36:03 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 AUTH_USER_MODEL = 'your_app.User'
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'dashboard'  # Will be overridden in views
-LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = 'login.html'
+LOGIN_REDIRECT_URL = 'dashboard.html'  # Will be overridden in views
+LOGOUT_REDIRECT_URL = 'index.html'
 
 # Add to middleware:
 MIDDLEWARE = [
